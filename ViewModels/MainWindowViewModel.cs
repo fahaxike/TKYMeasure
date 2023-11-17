da078b4cc541b587d429697129af91768035d98a
Administrator <Administrator@XTZJ-20230111FG>
2023-11-17 18:17:08 +0800

    添加项目文件。

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKYMeasure.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            Str = "bindtest";
+        }
+        private string _str;
+        public string Str
+        {
+            get { return _str; }
+            set { SetProperty(ref _str, value); }
+        }
+    }
+}
